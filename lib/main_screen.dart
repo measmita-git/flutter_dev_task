eb24cc31ec10be4f49d0d2ed26cc125e8a103821
unknown <rawalasmita87@gmail.com>
2023-12-12 14:23:23 +0545

    ui update

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -15,168 +15,170 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 20, left: 21, right: 18, bottom: 20),
-              child: SingleChildScrollView(
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // Existing content goes here
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20.0),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 50,
-                              height: 50,
-                              child: const CircleAvatar(
-                                backgroundImage: AssetImage('assets/asmita.jpg'),
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 68,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 18.0),
-                              child: Row(
-                                children: [
-                                  SvgPicture.asset('assets/forward.svg'),
-                                  SizedBox(width: 23,),
-                                  Text(
-                                    '02 Nov 2023',
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w400,
-                                        color: Color(0xff000000)),
-                                  ),
-                                  SizedBox(width: 23,),
-                                  SvgPicture.asset('assets/backward.svg'),
-                                ],
-                              ),
-                            ),
-                            Spacer(),
-
-                            SizedBox(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 20, left: 21, right: 18, bottom: 20),
+                child: SingleChildScrollView(
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Existing content goes here
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
                                 width: 50,
                                 height: 50,
-                                child: SvgPicture.asset('assets/notification.svg')),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 17,
-                      ),
-                      const SizedBox(
-                        width: 181,
-                        height: 44,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Hi Dr Leonardo!',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w700,
-                                color: Color(0xff000000),
-                              ),
-                            ),
-                            Text(
-                              'You have 4 appointments today.',
-                              style: TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w400,
-                                  color: Color(0xff868686)),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 22,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            prefixIcon: Icon(Icons.search),
-                            hintText: 'Search...'),
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      SizedBox(
-                        width: 385,
-                        height: 132,
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: SingleChildScrollView(
-                                child: Card(
-                                  child: PatientChart(),
+                                child: const CircleAvatar(
+                                  backgroundImage: AssetImage('assets/asmita.jpg'),
                                 ),
                               ),
-                            ),
-                            Expanded(
+                              const SizedBox(
+                                width: 68,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 18.0),
+                                child: Row(
+                                  children: [
+                                    SvgPicture.asset('assets/forward.svg'),
+                                    SizedBox(width: 23,),
+                                    Text(
+                                      '02 Nov 2023',
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w400,
+                                          color: Color(0xff000000)),
+                                    ),
+                                    SizedBox(width: 23,),
+                                    SvgPicture.asset('assets/backward.svg'),
+                                  ],
+                                ),
+                              ),
+                              Spacer(),
+
+                              SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: SvgPicture.asset('assets/notification.svg')),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 17,
+                        ),
+                        const SizedBox(
+                          width: 181,
+                          height: 44,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Hi Dr Leonardo!',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color(0xff000000),
+                                ),
+                              ),
+                              Text(
+                                'You have 4 appointments today.',
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xff868686)),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 22,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(Icons.search),
+                              hintText: 'Search...'),
+                        ),
+                        SizedBox(
+                          height: 25,
+                        ),
+                        SizedBox(
+                          width: 385,
+                          height: 132,
+                          child: Row(
+                            children: [
+                              Expanded(
                                 child: SingleChildScrollView(
                                   child: Card(
-                                    child:GenderBarGraph(),
+                                    child: PatientChart(),
                                   ),
-                                )),
-                          ],
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          Card(
-                            child: SizedBox(
-                                width: 333, height: 217, child: CandleChart()),
+                                ),
+                              ),
+                              Expanded(
+                                  child: SingleChildScrollView(
+                                    child: Card(
+                                      child:GenderBarGraph(),
+                                    ),
+                                  )),
+                            ],
                           ),
-                        ],
-                      ),
-                      SizedBox(
-                        width: 384,
-                        height: 233,
-                        child: Row(
+                        ),
+                        Column(
                           children: [
-                            Expanded(
-                              child: Card(
-
-                                child: PerformanceChart(),
-                              ),
-                            ),
-                            Expanded(
-                              child: Card(
-                                child: RadialBarChart(),
-                              ),
+                            Card(
+                              child: SizedBox(
+                                  width: 333, height: 217, child: CandleChart()),
                             ),
                           ],
                         ),
-                      ),
+                        SizedBox(
+                          width: 384,
+                          height: 233,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Card(
 
-                      Column(
-                        children: [
-                          Card(
-                            child: SizedBox(
-                                width: 333, height: 217, child: CurvyChart()),
+                                  child: PerformanceChart(),
+                                ),
+                              ),
+                              Expanded(
+                                child: Card(
+                                  child: RadialBarChart(),
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+
+                        Column(
+                          children: [
+                            Card(
+                              child: SizedBox(
+                                  width: 333, height: 217, child: CurvyChart()),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
 
 
-            SizedBox(height:50,child: ExpandableNavbar()),
-          ],
+              SizedBox(height:50,child: ExpandableNavbar()),
+            ],
+          ),
         ),
       ),
     );
--- lib/model/radial_chart.dart
+++ lib/model/radial_chart.dart
@@ -4,24 +4,43 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 class RadialBarChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SfCircularChart(
-      series: <CircularSeries>[
-        RadialBarSeries<ChartData, String>(
-          dataSource: [
-            ChartData('Ring 1', 200, 30, Color(0xFFC2A3FC), 'Depression'),
-            ChartData('Ring 2', 240, 80, Colors.blue, 'Anxiety Disorder'),
-            ChartData('Ring 3', 260, 100, Color(0xFFC2A3FC), 'Sleep Disorder'),
-            ChartData('Ring 4', 280, 20, Colors.orange, 'Stress Management'),
-            ChartData('Ring 5', 320, 140, Color(0xFFC2A3FC), 'Bipolar Disorder'),
+    return Scaffold(
 
-          ],
-          xValueMapper: (ChartData data, _) => data.category,
-          yValueMapper: (ChartData data, _) => data.startValue,
-          pointColorMapper: (ChartData data, _) => data.color,
-          innerRadius: '60%',
-          radius: '80%',
+
+        body:SizedBox(
+          child: SfCircularChart(
+            series: <CircularSeries>[
+              RadialBarSeries<ChartData, String>(
+                dataSource: [
+                  ChartData('Ring 1', 200, 30, Color(0xFFC2A3FC), 'Depression'),
+                  ChartData('Ring 2', 240, 80, Colors.blue, 'Anxiety Disorder'),
+                  ChartData('Ring 3', 260, 100, Color(0xFFC2A3FC), 'Sleep Disorder'),
+                  ChartData('Ring 4', 280, 20, Colors.orange, 'Stress Management'),
+                  ChartData('Ring 5', 320, 340, Color(0xFFC2A3FC), 'Bipolar Disorder'),
+
+                ],
+                xValueMapper: (ChartData data, _) => data.category,
+                yValueMapper: (ChartData data, _) => data.startValue,
+                pointColorMapper: (ChartData data, _) => data.color,
+                innerRadius: '40%',
+                enableTooltip: true,
+                radius: '90%',
+                maximumValue: 400,
+                dataLabelMapper: (ChartData data, _) => data.disorderType,
+                dataLabelSettings: const DataLabelSettings(
+                  // isVisible: true,
+
+
+
+                  labelPosition: ChartDataLabelPosition.outside,
+                  textStyle: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                ),
+              ),
+
+            ],
+          ),
         ),
-      ],
+
     );
   }
 }
